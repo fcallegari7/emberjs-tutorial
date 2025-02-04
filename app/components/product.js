515fd367791f51f5e814d058150b570f1c90f9d9
Fabio Callegari <fabio.callegari@fabio.callegari-WPQ7QD2TPF>
2025-02-04 15:24:54 -0800

    add helper for product data

--- app/components/product.js
+++ app/components/product.js
@@ -1,12 +1,5 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
 export default class Product extends Component {
-  @tracked number = 0;
-
-  @action
-  addNumber() {
-    this.number++;
-  }
+  productImage = this.args.product.colors[0].image;
 }
