045a253eac32b56646e9a099d6e1289f81bda8b2
Fabio Callegari <fabio.callegari@fabio.callegari-JFHY622JFX>
2025-03-13 14:18:01 -0700

    add tests

--- tests/integration/components/product/details-test.js
+++ tests/integration/components/product/details-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'emberjs-tutorial/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | product/details', function (hooks) {
@@ -9,18 +9,20 @@ module('Integration | Component | product/details', function (hooks) {
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set('price', {
+      original: 50,
+      current: 30,
+    });
+    this.set('onChangeColor', function (color) {
+      assert.equal(color, 'red');
+    });
+    this.set('colors', [{ color: 'red' }]);
+    await render(
+      hbs`<Product::Details @price={{this.price}} @colors={{this.colors}} @onChangeColor={{this.onChangeColor}} @isDetails={{true}} />`,
+    );
+    assert.dom('[data-test-original-price]').hasText('$50.00');
+    assert.dom('[data-test-current-price]').hasText('$30.00');
 
-    await render(hbs`<Product::Details />`);
-
-    assert.dom().hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <Product::Details>
-        template block text
-      </Product::Details>
-    `);
-
-    assert.dom().hasText('template block text');
+    await click('[data-test-color]');
   });
 });
